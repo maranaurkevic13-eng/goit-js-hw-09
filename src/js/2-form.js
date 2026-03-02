cf5fa8aeaf5ab9c761387215c2239e4ef2dec34c
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-03-02 13:30:04 +0200

    completed the tasks

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+let formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
+
+form.addEventListener("input", (event) => {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+
+    if (!formData.email || !formData.message) {
+        return "Fill please all fields";
+    }
+    console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+    formData = { email: "", message: "" };
+    form.reset();
+});
